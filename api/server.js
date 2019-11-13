f47e19091bd1a1ce04e47f1405e2a117fe907c4c
CarnunMP <carnun@hotmail.co.uk>
2019-11-13 14:12:00 +0000

    POST /api/register done

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const server = express();
+const Users = require('./users-model');
 
 server.use(helmet());
 server.use(express.json());
@@ -10,6 +14,20 @@ server.use(cors());
 
 server.get('/', (req, res) => {
   res.send("We're in business!");
+});
+
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 11);
+  const userToPost = { ...user, password: hash };
+
+  Users.add(userToPost)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
 });
 
 module.exports = server;
